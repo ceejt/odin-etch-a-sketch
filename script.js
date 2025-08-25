7b4376246c7418e1e35005893cf6fa930f76be8c
Cyril Josef Tinae <cyriljoseft@gmail.com>
2025-08-25 19:40:30 +0800

    Finish resize function - Added event listener when resize btn is clicked. - The function follows a for loop function to make individual boxes until the totalBox variable. - Adjusts CSS width and style of box based on the boxSize variable.

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ for (let i = 0; i < 256; i++) {
     div.classList.add('box');
     document.querySelector('.container').appendChild(div);
 }
+
+document.getElementById('resize').addEventListener('click', function resizeGrid() {
+    while (true) {
+        let newSize = prompt("Enter new grid size (max. 100): " );
+        
+        if (newSize === null) {
+            return;
+        }
+
+        if (newSize > 100) {
+            alert ("Size too large! Please enter a number less <= 100.");
+            continue;
+        } else {
+            const container = document.querySelector('.container');
+            container.textContent = '';
+            const containerSize = 480;
+            const boxSize = containerSize / newSize;
+            const totalBox = newSize * newSize;
+            
+            for (let i = 0; i < totalBox; i++) {
+                const div = document.createElement('div');
+                div.classList.add('box');
+                div.style.width = `${boxSize}px`;
+                div.style.height = `${boxSize}px`;
+                container.appendChild(div);
+            }
+            break;
+        }
+    }
+});
